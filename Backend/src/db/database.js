15e753df01c95213b47a909cf64420f74222c552
Asish2404 <asishbose313@gmail.com>
2026-07-30 17:22:53 +0530

    updaTION

--- Backend/src/db/database.js
+++ Backend/src/db/database.js
@@ -1,22 +1,17 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-
         const connectionInstance = await mongoose.connect(
-            `mongodb://127.0.0.1:27017/${DB_NAME}`
+            process.env.MONGODB_URI
         );
 
         console.log(
             `MongoDB Connected Successfully! HOST: ${connectionInstance.connection.host}`
         );
-
     } catch (error) {
-
         console.log("MongoDB Connection Error:", error);
         process.exit(1);
-
     }
 };
 
